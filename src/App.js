49c22accd077b19168fe5d834ad1fc640c02f0af
Stephen Phillips <stephen@wellthon.com>
2020-05-21 13:03:28 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
+import { findByLabelText } from '@testing-library/react';
+
+// const esc = encodeURIComponent;
+// export const queryFunc = (params) => Object.keys(params)
+//   .map(k => esc(k) + '=' + esc(params[k]))
+//   .join('&');
 
 function App() {
+
+  const [mountain, setMountain] = useState("");
+  const [mountainData, setMountainData] = useState("");
+  const [mountainError, setMountainError] = useState("");
+
+  const fetchResort = async () => {
+
+    const url = `https://liftie.info/api/resort/${mountain}`;
+
+    const response = await fetch(url, {mode: 'cors'});
+    if (!response.ok) {
+      console.log('Could not fetch user', response);
+      setMountainError(response);
+      setMountainData("");
+    }
+    setMountainData(response.json());
+    setMountainError("");
+  };
+
+
+  const handleChange = (event) => {
+    setMountain(event.target.value);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mountain Status</h1>
       </header>
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: '50px', maxWidth: '500px', margin: '50px', borderStyle: 'solid', borderWidth: '5px'}}>
+      <input className="question" name="name" id="nme" required autoComplete="off" type="text" onChange={handleChange} value={mountain} />
+      <label htmlFor="nme"><span>Enter mountain name?</span></label>
+      <button className="button" onClick={()=> {fetchResort()}} style={{width: '200px', height: '50px', backgroundColor: 'grey', color: 'white', marginTop: '30px'}}>Get Current Info</button>
+      </div>
+      <br />
+      <br />
+      <br />
+      {mountainData && <div>{mountainData}</div>}
+      {mountainError && <div>{mountainError}</div>}
     </div>
   );
 }
